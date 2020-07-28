64dff144f506dd9b2f5087a8f475a6147765526e
살균표백 <NobodyInbody@outlook.com>
2020-07-28 23:04:42 +0900

    aa

---
+++ AniTools.py
@@ -0,0 +1,29 @@
+import pymxs
+form Qt import QtWidgets
+RT = pymxs.runtime
+class bipedSelect(self):
+    m_biped_list = []
+    m_bone_list = []
+    m_bipName = ''
+    def __init__(self):
+        pass
+    def GetBipedBoneList(self, bipName):
+        pass
+        RootName = RT.getNodeByName(bipName)
+    def GetChindNode(self, node):
+        pass
+    def selectHead(self):
+        pass
+class animationRange(self):
+    m_animSet_list= []
+    def __init__(sefl):
+        pass
+    def add_animSet(sefl):
+        pass
+class MainWindow(self, QtWidgets.Qwidget):
+    m_maxScriptPath_str = u""
+    def __init__(self):
+        bip_ms = ''#
+        pass
+    def CreateWindows(self):
+        pass
